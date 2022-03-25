90d419400c453284e63fab7f664344c31f3aa31f
Dane-LeBlanc <dane.leblanc@gmail.com>
2022-03-24 21:30:13 -0600

    all tests passing

--- recursion.js
+++ recursion.js
@@ -41,16 +41,37 @@ function findIndex(arr, val, idx = 0) {
 
 /** revString: return a copy of a string, but in reverse. */
 
-function revString(str) {}
+function revString(str, idx = str.length - 1, newStr = "") {
+  if (idx < 0) return newStr;
+  newStr += str[idx];
+  return revString(str, idx - 1, newStr);
+}
 
 /** gatherStrings: given an object, return an array of all of the string values. */
 
-function gatherStrings(obj) {}
+function gatherStrings(obj) {
+  let stringArray = [];
+  for (let key in obj) {
+    if (typeof obj[key] === "string") stringArray.push(obj[key]);
+    if (typeof obj[key] === "object")
+      stringArray.push(...gatherStrings(obj[key]));
+  }
+  return stringArray;
+}
 
 /** binarySearch: given a sorted array of numbers, and a value,
  * return the index of that value (or -1 if val is not present). */
 
-function binarySearch(arr, val) {}
+function binarySearch(arr, val, left = 0, right = arr.length) {
+  if (left > right) return -1;
+  let mid = Math.floor((left + right) / 2);
+  if (arr[mid] === val) return mid;
+  if (arr[mid] < val) {
+    return binarySearch(arr, val, mid + 1, right);
+  } else {
+    return binarySearch(arr, val, left, mid - 1);
+  }
+}
 
 module.exports = {
   product,

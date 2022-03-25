6337d73a238368f58903b01e3a7171f213cd47ee
Dane-LeBlanc <dane.leblanc@gmail.com>
2022-03-24 20:23:38 -0600

    completed through findIndex

---
+++ recursion.js
@@ -0,0 +1,64 @@
+/** product: calculate the product of an array of numbers. */
+
+function product(nums, idx = 0) {
+  if (idx === nums.length) return 1;
+  return nums[idx] * product(nums, idx + 1);
+}
+
+/** longest: return the length of the longest word in an array of words. */
+
+function longest(words, idx = 0, longestSoFar = 0) {
+  if (idx === words.length) return longestSoFar;
+  longestSoFar = Math.max(words[idx].length, longestSoFar);
+  return longest(words, idx + 1, longestSoFar);
+}
+
+/** everyOther: return a string with every other letter. */
+
+function everyOther(str, idx = 0, newString = "") {
+  if (idx >= str.length) return newString;
+  newString += str[idx];
+  return everyOther(str, idx + 2, newString);
+}
+
+/** isPalindrome: checks whether a string is a palindrome or not. */
+
+function isPalindrome(str, idx = 0) {
+  let leftLetter = str[idx];
+  let rightLetter = str[str.length - 1 - idx];
+  if (idx > Math.floor(str.length / 2)) return true;
+  if (leftLetter !== rightLetter) return false;
+  return isPalindrome(str, idx + 1);
+}
+
+/** findIndex: return the index of val in arr (or -1 if val is not present). */
+
+function findIndex(arr, val, idx = 0) {
+  if (idx === arr.length) return -1;
+  if (arr[idx] === val) return idx;
+  return findIndex(arr, val, idx + 1);
+}
+
+/** revString: return a copy of a string, but in reverse. */
+
+function revString(str) {}
+
+/** gatherStrings: given an object, return an array of all of the string values. */
+
+function gatherStrings(obj) {}
+
+/** binarySearch: given a sorted array of numbers, and a value,
+ * return the index of that value (or -1 if val is not present). */
+
+function binarySearch(arr, val) {}
+
+module.exports = {
+  product,
+  longest,
+  everyOther,
+  isPalindrome,
+  findIndex,
+  revString,
+  gatherStrings,
+  binarySearch,
+};
